e31e69c62743e986c293652f593429add7072828
Satyasri Kar <satya.kar98@gmail.com>
2021-10-11 15:12:58 +0530

    initial commit

---
+++ src/main/kotlin/com/resource/horizon/ResourceApplication.kt
@@ -0,0 +1,11 @@
+package com.resource.horizon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResourceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResourceApplication>(*args)
+}
---
+++ src/test/kotlin/com/resource/horizon/ResourceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.resource.horizon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResourceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
