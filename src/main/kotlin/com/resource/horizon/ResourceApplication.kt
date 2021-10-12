568107c0057a6a3a69c0fcaf9cf41b0f79d6b926
Satyasri Kar <satya.kar98@gmail.com>
2021-10-12 09:05:25 +0530

    OpenApi 3.0 Swagger Added to Application

--- src/main/kotlin/com/resource/horizon/ResourceApplication.kt
+++ src/main/kotlin/com/resource/horizon/ResourceApplication.kt
@@ -1,17 +1,26 @@
 package com.resource.horizon
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = Info(
+        title = "Horizon Resource API",
+        version = "1.1.6",
+        description = "Information regarding Horizon Resource APIs"
+    )
+)
 class ResourceApplication
 
 var log: Logger = LoggerFactory.getLogger(ResourceApplication::class.java)
 
 
 fun main(args: Array<String>) {
-	runApplication<ResourceApplication>(*args)
-	log.info("Resource Application Started")
+    runApplication<ResourceApplication>(*args)
+    log.info("Resource Application Started")
 }
